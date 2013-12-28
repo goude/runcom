4f9333c7467bcd22f0ba1f169d527d364491f107
Daniel Goude <daniel@goude.se>
2013-12-28 23:28:23 +0000

    Add vim installation

--- install.sh
+++ install.sh
@@ -4,9 +4,15 @@ REPOS=$HOME/.homesick/repos
 git clone https://github.com/goude/homeshick.git $REPOS/homeshick
 source $REPOS/homeshick/homeshick.sh
 
+echo "Cloning homeshick repos..."
 homeshick clone https://github.com/robbyrussell/oh-my-zsh
 homeshick clone https://github.com/goude/runcom.git
-homeshick clone https://github.com/goude/vim-files.git
 homeshick clone https://github.com/goude/yaprox.git
+homeshick clone https://github.com/goude/vim-files.git
 
 homeshick --force link
+
+echo "Setting up Vim..."
+source $REPOS/vim-files/install.sh
+
+echo "Done."

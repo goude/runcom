1d5fa29a68eb51ab3a751512a6ffb3955bd3a6c7
Daniel Goude <daniel@goude.se>
2014-05-10 17:24:07 +0200

    add git default setting

--- home/bin/goude-setup-git.sh
+++ home/bin/goude-setup-git.sh
@@ -3,5 +3,6 @@ echo "Setting up default git configuration."
 
 git config --global user.name "Daniel Goude"
 git config --global user.email "daniel@goude.se"
+git config --global push.default simple
 
 git config --list

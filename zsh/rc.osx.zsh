9585a7ef3eaca1a4d87d79cb579fee7a7647a26b
Daniel Goude <daniel@goude.se>
2014-07-05 23:31:40 +0200

    improve boxen env sourcing

--- zsh/rc.osx.zsh
+++ zsh/rc.osx.zsh
@@ -2,6 +2,7 @@
 
 export CLICOLOR=1
 export LSCOLORS="gxfxcxdxbxegedabagacad"
+export HOMEBREW_BUILD_FROM_SOURCE=1
 
 #bindkey "^[[3~" delete-char
 
@@ -15,7 +16,7 @@ if [[ -z "$LANG" ]]; then
     export LANG='en_US.UTF-8'
 fi
 
-source /opt/boxen/env.sh
+[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 #echo "JAVA_HOME set to $JAVA_HOME"

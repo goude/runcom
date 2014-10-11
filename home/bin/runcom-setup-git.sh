bce1c75f41e76e5f75bcacd827c7ca932db580be
Daniel Goude <daniel@goude.se>
2014-10-11 09:52:51 +0200

    fix setup

--- home/bin/runcom-setup-git.sh
+++ home/bin/runcom-setup-git.sh
@@ -5,9 +5,14 @@ git config --global user.name "Daniel Goude"
 git config --global user.email "daniel@goude.se"
 git config --global push.default simple
 
-# http://usevim.com/2012/03/21/git-and-vimdiff/
 git config --global diff.tool vimdiff
 git config --global merge.tool vimdiff
 #git config --global difftool.prompt false
 
+git config --global --unset http.sslVerify
+git config --global --unset http.sslCAinfo
+
 git config --list
+
+
+

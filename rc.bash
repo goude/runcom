f26802e2ee68b09c0628fdddc618029e4f73e122
Daniel Goude <daniel@goude.se>
2015-04-08 21:56:46 +0200

    refactor zsh/bash rcs

--- rc.bash
+++ rc.bash
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
-# Path to the bash it configuration
+source $HOME/.homesick/repos/runcom/rc.common
+
+# Bash-it configuration
 export BASH_IT=$HOME/.homesick/repos/bash-it
-
-# Lock and Load a custom theme file
-# location /.bash_it/themes/
-#export BASH_IT_THEME='bobby'
 export BASH_IT_THEME='demula'
-
-# Your place for hosting Git repos. I use this for private repos.
-export GIT_HOSTING='git@git.domain.com'
-
-# Don't check mail when opening terminal.
-unset MAILCHECK
-
-# Change this to your console based IRC client of choice.
-export IRC_CLIENT='irssi'
-
-# Set this to the command you use for todo.txt-cli
-export TODO="t"
 
 # Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
@@ -26,5 +12,13 @@ export SCM_CHECK=true
 # https://github.com/xvzf/vcprompt
 #export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
 
+# Your place for hosting Git repos. I use this for private repos.
+#export GIT_HOSTING='git@git.domain.com'
+#unset MAILCHECK
+#export IRC_CLIENT='irssi'
+#export TODO="t"
+
 # Load Bash It
 source $BASH_IT/bash_it.sh
+
+source $HOME/.homesick/repos/runcom/rc.common-post
--- rc.zsh
+++ rc.zsh
@@ -1,17 +1,5 @@
-#!/bin/zsh
-homeshick_repos=$HOME/.homesick/repos
-rcfiles=$HOME/.homesick/repos/runcom
-system=`$rcfiles/system`
-
-# common environment variables
-export PATH=$PATH:$HOME/.node/bin:$HOME/bin
-export PYTHONSTARTUP=$HOME/.pystartup
-export WORKON_HOME=$HOME/.virtualenvs
-export EDITOR=vim
-
-# homeshick - git dotfile synchronizer
-source $HOME/.homesick/repos/homeshick/homeshick.sh
-fpath=($HOME/.homesick/repos/homeshick/completions $fpath)
+#!/usr/bin/env zsh
+source $HOME/.homesick/repos/runcom/rc.common
 
 # oh-my-zsh configuration
 omz_dir=$HOME/.homesick/repos/oh-my-zsh
@@ -59,72 +47,10 @@ bindkey "^H" backward-delete-char      # Control-h also deletes the previous cha
 bindkey "^U" kill-line
 bindkey '^R' history-incremental-search-backward
 
-if [[ $system == 'Linux' ]]; then
-    alias ls='ls -h --color=auto'
-    alias rm='rm -I'
-    alias grep='grep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    #export TERM="xterm-256color"
-    #export JAVA_HOME=/opt/java/jre1.7.0/
-    #export PATH=/opt/node/bin:$PATH
-fi
-if [[ $system == 'OSX' ]]; then
-    export CLICOLOR=1
-    export LSCOLORS="gxfxcxdxbxegedabagacad"
-    #export HOMEBREW_BUILD_FROM_SOURCE=1
-
-    #bindkey "^[[3~" delete-char
-
-    if [[ -x /usr/local/bin/gls ]]; then
-        alias ls='/usr/local/bin/gls -h --color=auto'
-    else
-        alias ls='ls -hG'
-    fi
-
-    if [[ -z "$LANG" ]]; then
-        export LANG='en_US.UTF-8'
-    fi
-fi
-if [[ $system == 'Cygwin' ]]; then
-    source $HOME/.homesick/repos/runcom/mintty/sol.dark
-fi
-
-source $rcfiles/aliases
-source $rcfiles/functions
-
 # Vim's text-objects-ish for zsh
 if [[ -f $HOME/.homesick/repos/opp.zsh/opp.zsh ]]; then
     source $HOME/.homesick/repos/opp.zsh/opp.zsh
     source $HOME/.homesick/repos/opp.zsh/opp/*.zsh
-fi
-
-# Tmux improver
-if [[ -d $HOME/.homesick/repos/tmuxifier/bin ]]; then
-  export TMUXIFIER_LAYOUT_PATH="$HOME/.tmux-layouts"
-  PATH=$PATH:$HOME/.homesick/repos/tmuxifier/bin
-  eval "$(tmuxifier init -)"
-fi
-
-if [[ -f /usr/share/virtualenvwrapper/virtualenvwrapper.sh ]]; then
-    source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-fi
-
-# Proxy helper
-if [[ -f $HOME/.homesick/repos/yaprox/yaprox.sh ]]; then
-    source $HOME/.homesick/repos/yaprox/yaprox.sh
-fi
-
-if [[ -f ~/.dir_colors && ( -x /usr/local/bin/dircolors || -x /usr/bin/dircolors ) ]]; then
-    eval `dircolors $HOME/.dir_colors`
-fi
-
-if [[ -e ~/.ssh/ssh_auth_sock ]] then
-    export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
-fi
-
-if [ -f ~/.localenv ]; then
-    source ~/.localenv
 fi
 
 # -o - allow long option names
@@ -138,15 +64,4 @@ setopt -o sharehistory
 # Disable Ctrl-Z
 set -o ignoreeof
 
-# Turn off Ctrl-s XOFF
-stty ixany
-stty ixoff -ixon
-stty stop undef
-stty start undef
-
-# Remove duplicates from environment vars
-typeset -U PATH # remove duplicates
-
-unset rcfiles
-unset system
-unset homeshick_repos
+source $HOME/.homesick/repos/runcom/rc.common-post

62554613518d3f58ff8927246bd04d7bf85704d3
Daniel Goude <daniel@goude.se>
2015-07-25 12:17:31 +0200

    hb

---
+++ heartbeat/heartbeat.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+from goulog import log_event
+import socket
+import json
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("gmail.com",80))
+    return s.getsockname()[0]
+
+def main():
+    hbi = {
+        'hostname': socket.gethostname(),
+        'ip': get_ip(),
+    }
+
+    hbs = json.dumps(hbi)
+    #print(hbs)
+    log_event(name=u'heartbeat.info', value=hbs, unit='json', ip=hbi['ip'])
+
+if __name__ == '__main__':
+    main()

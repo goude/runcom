28b8f5412aace3f6c8a1924b7f53a97fc0b3fed8
Daniel Goude <daniel@goude.se>
2015-08-09 23:08:27 +0200

    tweaks

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -44,6 +44,7 @@
 (add-to-list 'default-frame-alist '(width . 120))
 
 (global-linum-mode t)
+(global-hl-line-mode t)
 
 (display-time)
 
@@ -85,18 +86,10 @@
 	'(normal insert))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
 
 ;;(load-theme 'tango-dark)

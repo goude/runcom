3445df084863b5bcdb5f177a7107f4c313d62306
Daniel Goude <daniel@goude.se>
2015-09-08 00:01:17 +0200

    cleanup

--- setup_homeshick.sh
+++ setup_homeshick.sh
@@ -7,26 +7,18 @@ source $REPOS/homeshick/homeshick.sh
 echo "Cloning homeshick repos..."
 
 homeshick_repos=(
-    #"robbyrussell/oh-my-zsh"
     "goude/prezto"
     "goude/runcom"
+    "goude/thxtime"
     "goude/yaprox"
     "spf13/spf13-vim"
-    #"hchbaw/opp.zsh"
     #"jimeh/tmuxifier"
 )
 
 for i in "${homeshick_repos[@]}"
 do
     homeshick --force --batch clone $i
-    #if [ -d "$REPOS/$i" ]; then
-        #echo "$i exists. Doing nothing."
-    #else
-        #echo "$i does not exist. Doing nothing."
-    #fi
 done
-
-#git clone --depth=1 https://github.com/Bash-it/bash-it.git $REPOS/bash-it
 
 echo "Silently and forcefully linking homeshick..."
 homeshick --force --batch --quiet link

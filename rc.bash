a85418119cdb68b5d3b3c00ed54c34101087d57a
Daniel Goude <daniel@goude.se>
2015-12-07 23:06:44 +0100

    improve bash PS1

--- rc.bash
+++ rc.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source $HOME/.homesick/repos/runcom/rc.common
-export PS1='\w λ '
+export PS1='\e[0;31m\u\e[0m@\e[0;33m\h\e[0m \e[0;90m\W λ\e[0m '
 source $HOME/.homesick/repos/runcom/rc.common-post

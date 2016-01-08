c589e31a7b7e43a85f3b80e5d4d50c0314f2d84b
Daniel Goude <daniel@goude.se>
2016-01-08 13:44:07 +0100

    custom prompt

--- rc.bash
+++ rc.bash
@@ -10,7 +10,7 @@ GIT_PROMPT_ONLY_IN_REPO=1
 # GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
 # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
 # as last entry source the gitprompt script
-# GIT_PROMPT_THEME=Custom # use custom .git-prompt-colors.sh
+GIT_PROMPT_THEME=Custom # use custom .git-prompt-colors.sh
 # GIT_PROMPT_THEME=Solarized # use theme optimized for solarized color scheme
 source ~/.bash-git-prompt/gitprompt.sh
 

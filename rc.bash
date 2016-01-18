cc595873bc21a4c36d9ce8f1b318ede2b55296da
Daniel Goude <daniel@goude.se>
2016-01-18 17:28:46 +0100

    ud bash prompt

--- rc.bash
+++ rc.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 source $HOME/.homesick/repos/runcom/rc.common
 
+source $HOME/.homesick/repos/runcom/git/git-completion.bash
+source $HOME/.homesick/repos/runcom/git/git-prompt.sh
 source $HOME/.homesick/repos/runcom/bash_colors
-export PS1="\[$Red\]\u\[$CReset\]@\[$Yellow\]\h\[$CReset\] \[$Blue\]\W\[$CReset\] \[$Green\]λ\[$CReset\] "
+export PS1="\$(__git_ps1 '[%s]')\\n\[$Red\]\u\[$CReset\]@\[$Yellow\]\h\[$CReset\] \[$Blue\]\W\[$CReset\] \[$Green\]λ\[$CReset\] "
 
 source $HOME/.homesick/repos/runcom/rc.common-post

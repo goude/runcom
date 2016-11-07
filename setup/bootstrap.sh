aac7c4ac72cd7a54368e26069f8534101ad1509c
Daniel Goude <daniel@goude.se>
2016-11-07 22:49:43 +0100

    fix paths

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -2,7 +2,7 @@
 echo "Bootstrapping runcom..."
 
 mkdir -p "$HOME/tmp"
-cd "$HOME/tmp"
+cd "$HOME/tmp" || exit
 git clone https://github.com/goude/runcom.git
-cd runcom/setup
+cd runcom/setup || exit
 ./install.sh

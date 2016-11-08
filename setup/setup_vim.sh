31e39490ba3c44ab8bd2922d3afed019b4d1639e
Daniel Goude <daniel@goude.se>
2016-11-08 20:00:55 +0100

    tweaks for quitting

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -4,4 +4,4 @@ echo "Setting up Vim..."
 REPOS=$HOME/.homesick/repos
 mkdir -p $REPOS/spf13-vim/.vim/bundle
 git clone --recursive 'https://github.com/gmarik/vundle.git' $HOME/.vim/bundle/vundle
-vim +BundleInstall! +BundleClean +q
+vim +BundleInstall! +BundleClean! +qall! +q! +q!

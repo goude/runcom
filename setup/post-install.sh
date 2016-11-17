c2117737574779380cbc7a6f5b894fae268660f8
Daniel Goude <daniel@goude.se>
2016-11-17 23:45:28 +0100

    force antigen post-install

--- setup/post-install.sh
+++ setup/post-install.sh
@@ -2,5 +2,9 @@
 echo "Performing post-installation..."
 
 ../home/bin/runcom-setup-git
+
 ln -s $HOME/.tmux.conf-template $HOME/.tmux.conf
 ln -s $HOME/.npmrc-template $HOME/.npmrc
+
+# Force antigen install
+zsh -c "source $HOME/.zshrc"

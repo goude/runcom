c23290e411d530bf0b88fb4605527e0d2f7b35ef
Daniel Goude <daniel@goude.se>
2017-02-09 11:14:51 +0100

    add zsh profiling (off) and turn off sdkman for now (slow)

--- rc.zsh
+++ rc.zsh
@@ -1,5 +1,16 @@
 #!/usr/bin/env zsh
 
+# begin profiling
+# https://kev.inburke.com/kevin/profiling-zsh-startup-time/
+PROFILE_STARTUP=false
+if [[ "$PROFILE_STARTUP" == true ]]; then
+    # http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
+    PS4=$'%D{%M%S%.} %N:%i> '
+    exec 3>&2 2>$HOME/tmp/startlog.$$
+    setopt xtrace prompt_subst
+fi
+# end profiling (see also section at end)
+
 # FIXME: There are still some things in .zprofile, check these.
 export RPROMPT=" "
 export DISABLE_AUTO_TITLE="true"
@@ -125,3 +136,10 @@ antigen apply
 
 # Finally, source common-post
 source $HOME/.homesick/repos/runcom/rc.common-post
+
+# cleanup profiling
+if [[ "$PROFILE_STARTUP" == true ]]; then
+    unsetopt xtrace
+    exec 2>&3 3>&-
+fi
+# end cleanup profiling

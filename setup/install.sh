35093be8a27c0d7ae6d73fa98f42d3ff5b647e4f
Daniel Goude <daniel@goude.se>
2017-03-25 22:26:43 +0100

    ud install process

--- setup/install.sh
+++ setup/install.sh
@@ -2,5 +2,7 @@
 echo "Installing/updating runcom..."
 
 ./setup_homeshick.sh
-./setup_vim.sh
+#./setup_vim.sh
+./setup-pyenv.sh
+./setup-neovim.sh
 
--- setup/setup-neovim.sh
+++ setup/setup-neovim.sh
@@ -12,17 +12,17 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 pyenv install 3.4.4
 pyenv virtualenv 3.4.4 neovim3
 pyenv activate neovim3
-pip3.4 install neovim
-pip3.4 install flake8 vim-vint yamllint
+pip3.4 install --upgrade pip
+pip3.4 install neovim flake8 vim-vint yamllint
 npm install -g htmlhint write-good jsonlint
 ln -s `pyenv which flake8` ~/bin/flake8
 pyenv deactivate
 
-pyenv install 2.7.11
-pyenv virtualenv 2.7.11 neovim2
-pyenv activate neovim2
-unalias pip
-pip install neovim
-pyenv deactivate
+#pyenv install 2.7.11
+#pyenv virtualenv 2.7.11 neovim2
+#pyenv activate neovim2
+#unalias pip
+#pip install neovim
+#pyenv deactivate
 
 nvim +PlugInstall +q +q

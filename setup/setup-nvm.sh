bcbd83249804af153b60a2a4695f9aeed3c046c3
Daniel Goude <daniel@goude.se>
2017-04-21 16:09:10 +0200

    nvm

---
+++ setup/setup-nvm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# change to clone repo instead, see instructions at
+# https://github.com/creationix/nvm/blob/master/README.markdown#install-script
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash

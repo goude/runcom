a2b8e359ba28e8fe9fdd9bdcfdcc980429e76f64
Daniel Goude <daniel@goude.se>
2017-04-24 14:45:10 +0200

    rm deprecated

--- deprecated/build-youcompleteme-on-old-machine.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd ~/.vim/bundle && rm -rf YouCompleteMe && git clone https://github.com/Valloric/YouCompleteMe.git && cd YouCompleteMe && git reset --hard 0de1c0c && git submodule update --init --recursive && ./install.py
--- deprecated/setup_vim.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Setting up Vim..."
-
-REPOS=$HOME/.homesick/repos
-mkdir -p $REPOS/spf13-vim/.vim/bundle
-git clone --recursive 'https://github.com/gmarik/vundle.git' $HOME/.vim/bundle/vundle
-vim +BundleInstall! +BundleClean! +qall! +q! +q!

3a887fc1e8c28e318158acaec3b6acd4e76565fd
Daniel Goude <daniel@goude.se>
2017-06-04 13:14:36 +0200

    ud rbenv setup

--- setup/setup-rbenv.sh
+++ setup/setup-rbenv.sh
@@ -2,4 +2,5 @@
 
 cd $HOME
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 cd ~/.rbenv && src/configure && make -C src

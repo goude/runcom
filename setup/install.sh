2342703f8a1906772f8ba8fbc43b15ae55c258ec
Daniel Goude <daniel@goude.se>
2017-08-30 15:11:49 +0200

    source theme

--- setup/install.sh
+++ setup/install.sh
@@ -9,7 +9,7 @@ ln -s $HOME/.tmux.conf-template $HOME/.tmux.conf
 
 # Set initial color scheme
 ln -s $HOME/.local/share/base16/templates/shell/scripts/base16-gruvbox-dark-soft.sh $HOME/.base16_theme
-$HOME/.base16_theme
+source $HOME/.base16_theme
 
 ./install-prog-envs.sh
 

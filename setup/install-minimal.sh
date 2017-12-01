2ce96b10c94657f39ffa0e5a2787f06bb6dcc8ff
kswg248 <daniel.goude@astrazeneca.com>
2017-12-01 12:14:22 +0100

    update install

--- setup/install-minimal.sh
+++ setup/install-minimal.sh
@@ -4,9 +4,9 @@ echo "Installing minimal environment..."
 
 ./setup-homeshick.sh
 
-sudo APT_CONFIG=/home/admin/.apt.conf apt-get update
-sudo APT_CONFIG=/home/admin/.apt.conf apt-get upgrade
-sudo APT_CONFIG=/home/admin/.apt.conf apt-get install tmux zsh tree cifs-utils
+sudo -E apt-get update
+sudo -E apt-get upgrade
+sudo -E apt-get install tmux zsh tree cifs-utils
 
 ./setup-pyenv.sh
 ./setup-pyenvs.sh

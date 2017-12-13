c6849d754fd4d8572cc2d4c5b9ac0fec3cc5f6dd
Daniel Goude <daniel@goude.se>
2017-12-13 18:46:35 +0100

    tweak setup

--- setup/install-minimal.sh
+++ setup/install-minimal.sh
@@ -7,6 +7,8 @@ echo "Installing minimal environment..."
 sudo -E apt-get update
 sudo -E apt-get upgrade
 sudo -E apt-get install tmux zsh tree cifs-utils
+# Libs needed to build python packages
+sudo -E apt-get install libbz2-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libxslt-dev
 
 ./setup-pyenv.sh
 ./setup-pyenvs.sh

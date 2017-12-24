cc95d00115ba8356e1d69be7266db4adf972277a
Daniel Goude <daniel@goude.se>
2017-12-24 19:07:53 +0100

    add todopy

--- setup/update-pyenv-packages.sh
+++ setup/update-pyenv-packages.sh
@@ -6,13 +6,23 @@ if [[ -d $HOME/.pyenv/bin ]]; then
   eval "$(pyenv virtualenv-init -)"
 fi
 
+# Python 3 libs and command line tools
 pyenv activate neovim3
 
 $(pyenv which pip) install --upgrade jedi flake8 vim-vint yamllint requests pillow numpy click tmuxp jrnl autopep8
 
-ln -s "$(pyenv which flake8)" ~/bin/flake8
-ln -s "$(pyenv which tmuxp)" ~/bin/tmuxp
-ln -s "$(pyenv which jrnl)" ~/bin/jrnl
+ln -sf "$(pyenv which flake8)" ~/bin/flake8
+ln -sf "$(pyenv which tmuxp)" ~/bin/tmuxp
+ln -sf "$(pyenv which jrnl)" ~/bin/jrnl
 
 pyenv rehash
+pyenv deactivate
+
+
+# Python 2 libs and command line tools
+pyenv activate neovim2
+
+$(pyenv which pip) install --upgrade todopy
+ln -sf "$(pyenv which todopy)" ~/bin/todopy
+
 pyenv deactivate

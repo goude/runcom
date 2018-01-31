27ac06d59080c04847520b0854c5a06e8e0943d8
Daniel Goude <daniel@goude.se>
2018-01-31 21:56:43 +0100

    add neovim npm package

--- setup/update-node-packages.sh
+++ setup/update-node-packages.sh
@@ -2,7 +2,7 @@
 source "$HOME/.nvm/nvm.sh"
 nvm use node
 
-npm install -g npm@latest htmlhint write-good jsonlint base16-builder-node eslint eslint-plugin-react tern tldr
+npm install -g npm@latest htmlhint write-good jsonlint base16-builder-node eslint eslint-plugin-react tern tldr neovim
 
 # TODO: check https://github.com/sindresorhus/awesome-nodejs
 

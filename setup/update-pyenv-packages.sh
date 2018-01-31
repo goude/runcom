1149fe8270bc290b61c0d70972bc4dd34140c8ce
Daniel Goude <daniel@goude.se>
2018-01-31 21:22:39 +0100

    add mypy/yapf symlinks

--- setup/update-pyenv-packages.sh
+++ setup/update-pyenv-packages.sh
@@ -9,7 +9,7 @@ fi
 # Python 3 libs and command line tools
 pyenv activate neovim3
 
-$(pyenv which pip) install --no-cache-dir --upgrade jedi flake8 vim-vint yamllint requests pillow numpy click tmuxp jrnl autopep8 topydo mypy
+$(pyenv which pip) install --no-cache-dir --upgrade jedi flake8 vim-vint yamllint requests pillow numpy click tmuxp jrnl autopep8 topydo mypy yapf
 $(pyenv which pip) install topydo[columns]
 $(pyenv which pip) install topydo[ical]
 
@@ -17,6 +17,8 @@ ln -sf "$(pyenv which flake8)" ~/bin/flake8
 ln -sf "$(pyenv which tmuxp)" ~/bin/tmuxp
 ln -sf "$(pyenv which jrnl)" ~/bin/jrnl
 ln -sf "$(pyenv which topydo)" ~/bin/topydo
+ln -sf "$(pyenv which mypy)" ~/bin/mypy
+ln -sf "$(pyenv which mypy)" ~/bin/yapf
 
 pyenv rehash
 pyenv deactivate

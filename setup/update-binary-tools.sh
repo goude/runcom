14d9290e6ba56baad2b4b88348aca4718c4d1844
Daniel Goude <daniel@goude.se>
2018-04-28 14:16:14 +0200

    create ~/bin if it does not exist

--- setup/update-binary-tools.sh
+++ setup/update-binary-tools.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Installing ~/bin/nvim..."
+mkdir -p ~/bin
 curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.2.2/nvim.appimage'
 chmod a+rx "$HOME/bin/nvim"

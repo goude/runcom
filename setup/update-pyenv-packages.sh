e069d3cedde72088d4abdc2cec8b10699d1c0e0e
Daniel Goude <daniel@goude.se>
2018-06-19 19:41:55 +0200

    fix update-pyenv-packages

--- setup/update-pyenv-packages.sh
+++ setup/update-pyenv-packages.sh
@@ -12,14 +12,31 @@ pyenv activate neovim3
 install_command="$(pyenv which pip) install --no-cache-dir --upgrade"
 
 $install_command pip
-$install_command jedi flake8 vim-vint yamllint requests pillow numpy click tmuxp autopep8 topydo mypy yapf neovim pipenv humanfriendly
+$install_command \
+  autopep8 \
+  click \
+  flake8 \
+  humanfriendly \
+  jedi \
+  mypy \
+  neovim \
+  numpy \
+  pillow \
+  pipenv \
+  requests \
+  tmuxp \
+  topydo \
+  vim-vint \
+  yamllint \
+  yapf
+
 $install_command topydo[columns] topydo[ical]
 
 pyenv rehash
 
 ln -sf "$(pyenv which flake8)" ~/bin/flake8
 ln -sf "$(pyenv which mypy)" ~/bin/mypy
-ln -sf "$(pyenv which mypy)" ~/bin/yapf
+ln -sf "$(pyenv which yapf)" ~/bin/yapf
 ln -sf "$(pyenv which tmuxp)" ~/bin/tmuxp
 ln -sf "$(pyenv which topydo)" ~/bin/topydo
 ln -sf "$(pyenv which pipenv)" ~/bin/pipenv

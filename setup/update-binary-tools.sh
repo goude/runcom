ff0840e1f7650ba26827e28f8e8061a8344be075
Daniel Goude <daniel@goude.se>
2018-07-09 14:07:59 +0200

    use nvim 0.3.0 appimage

--- setup/update-binary-tools.sh
+++ setup/update-binary-tools.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing ~/bin/nvim..."
 mkdir -p ~/bin
-curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.2.2/nvim.appimage'
+#curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.2.2/nvim.appimage'
+curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.3.0/nvim.appimage'
+
 chmod a+rx "$HOME/bin/nvim"

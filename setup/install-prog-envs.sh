3adce1fe53a934735ca20ffc65a633e0404dc6b1
Daniel Goude <daniel@goude.se>
2018-08-02 22:41:15 +0200

    add noninteractive flag and reorder install-prog-envs

--- setup/install-prog-envs.sh
+++ setup/install-prog-envs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Setting up programming environments..."
 
+./setup-nvm.sh
+./update-node-packages.sh
+
 ./setup-pyenv.sh
 ./setup-pyenvs.sh
 ./update-pyenv-packages.sh
 ./setup-neovim.sh
-
-./setup-nvm.sh
-./update-node-packages.sh
 
 ./setup-rbenv.sh
 ./setup-ruby.sh

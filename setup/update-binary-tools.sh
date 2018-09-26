f87833ec8233db7c4254d87f84107bc88796ad59
Daniel Goude <daniel@goude.se>
2018-09-26 18:53:15 +0200

    update nvim downloader

--- setup/update-binary-tools.sh
+++ setup/update-binary-tools.sh
@@ -2,6 +2,7 @@
 echo "Installing ~/bin/nvim..."
 mkdir -p ~/bin
 #curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.2.2/nvim.appimage'
-curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.3.0/nvim.appimage'
+#curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.3.0/nvim.appimage'
+curl -L -o "$HOME/bin/nvim" --silent 'https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage'
 
 chmod a+rx "$HOME/bin/nvim"

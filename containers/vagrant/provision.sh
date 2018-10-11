e1840b861191f4b7c4d5bbbc8a5c9f554d09307e
Daniel Goude <daniel@goude.se>
2018-10-11 19:47:41 +0200

    update build files

--- containers/vagrant/provision.sh
+++ containers/vagrant/provision.sh
@@ -13,43 +13,9 @@ apt-get -y install $PACKAGES
   #build-essential \
   #ca-certificates \
   #cmake \
-  #git \
   #hdevtools \
   #linux-image-extra-$(uname -r) \
   #linux-image-extra-virtual \
-  #software-properties-common \
-  #tlsdate \
-  #tmux \
-  #vim-nox \
-  #zsh \
-  #exuberant-ctags \
-  #silversearcher-ag \
-  #curl \
-  #htop \
-  #jq \
-  #neovim \
-  #tree \
-  #libbz2-dev \
-  #libreadline-dev \
-  #libcurl4-openssl-dev \
-  #libsqlite3-dev \
-  #libssl-dev \
-  #libxml2-dev \
-  #libxslt-dev \
-  #ghc \
-  #pandoc \
-  #cifs-utils \
-  #openjdk-8-jre-headless \
-  #postgresql-client
-
-#python-dev \
-#python3-cairo \
-#python3-dev \
-#python3-lxml \
-#python3-matplotlib \
-#python3-pandas \
-#python3-pip \
-#python3-venv \
 
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -

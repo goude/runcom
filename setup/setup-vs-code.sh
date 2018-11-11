f7d73e33c9ff4da1816d3d4ea64123c6d2a4585b
Daniel Goude <daniel@goude.se>
2018-11-11 14:30:39 +0100

    add setup-vs-code

---
+++ setup/setup-vs-code.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code

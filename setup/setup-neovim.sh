b9567424348dbd108928b4c8f9a6e21fe98dfb32
Daniel Goude <daniel@goude.se>
2018-11-29 22:13:39 +0100

    attempt to fix nvim installation in Docker

--- setup/setup-neovim.sh
+++ setup/setup-neovim.sh
@@ -4,4 +4,5 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #nvim +PlugInstall +q +q
-nvim +PlugInstall +q +q &> /dev/null < /dev/tty
+#nvim +PlugInstall +q +q &> /dev/null < /dev/tty
+nvim +PlugInstall +qall &> /dev/null < /dev/tty

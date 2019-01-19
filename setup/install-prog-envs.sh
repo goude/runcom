8e5a2d93d342fdfeba9c6f211aab209cedb58194
Daniel Goude <daniel@goude.se>
2019-01-20 00:38:29 +0100

    pipenv prep

--- setup/install-prog-envs.sh
+++ setup/install-prog-envs.sh
@@ -8,8 +8,12 @@ source ../localenv
 ./update-node-packages.sh
 
 ./setup-pyenv.sh
-./setup-pyenvs.sh
-./update-pyenv-packages.sh
+#./setup-pyenvs.sh
+#./update-pyenv-packages.sh
+(cd rcpy && ./setup.sh)
+(cd rcpy/python3 && ./setup.sh)
+(cd rcpy/python2 && ./setup.sh)
+
 ./setup-neovim.sh
 
 ./setup-rbenv.sh

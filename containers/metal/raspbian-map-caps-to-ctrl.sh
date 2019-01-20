ee68484650ad61c1d076b1b90908391d80bf3168
Daniel Goude <daniel@goude.se>
2019-01-20 20:40:19 +0100

    add raspbian instructions for mapping caps->ctrl

---
+++ containers/metal/raspbian-map-caps-to-ctrl.sh
@@ -0,0 +1,8 @@
+#/etc/default/keyboard
+
+#XKBMODEL="pc105"
+#XKBLAYOUT="us"
+#XKBVARIANT="altgr-intl"
+#XKBOPTIONS="terminate:ctrl_alt_bksp,ctrl:nocaps"
+
+#sudo dpkg-reconfigure keyboard-configuration

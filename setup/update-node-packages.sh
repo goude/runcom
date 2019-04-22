76ca53fa84e841b65706aa7b5ec65b6bd7f93d5a
Daniel Goude <daniel@goude.se>
2019-04-22 08:42:23 +0200

    nvim updates

--- setup/update-node-packages.sh
+++ setup/update-node-packages.sh
@@ -2,7 +2,8 @@
 source "$HOME/.nvm/nvm.sh"
 nvm use node
 
-npm install -g npm@latest htmlhint write-good jsonlint base16-builder-node eslint eslint-plugin-react tern tldr neovim prettier stylelint
+npm install -g npm@latest jsonlint base16-builder-node eslint eslint-plugin-react tern tldr neovim prettier stylelint
+#npm install -g npm@latest htmlhint jsonlint base16-builder-node eslint eslint-plugin-react tern tldr neovim prettier stylelint
 
 # TODO: check https://github.com/sindresorhus/awesome-nodejs
 

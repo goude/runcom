76254abe1e80e499123ba254c0565a219deeca93
Daniel Goude <daniel@goude.se>
2019-09-14 23:15:08 +0200

    homebrew updates

--- setup/rcpy/python2/setup.sh
+++ setup/rcpy/python2/setup.sh
@@ -2,7 +2,7 @@
 source "$HOME/.homesick/repos/runcom/localenv"
 
 $RUNCOM_PIPENV_CMD --python 2
-ln -sf "$($RUNOM_PIPENV_CMD run pyenv which python2)" ~/bin/neovim-python2
+ln -sf "$($RUNCOM_PIPENV_CMD run pyenv which python2)" ~/bin/neovim-python2
 
 $RUNCOM_PIPENV_CMD install neovim
 
--- setup/rcpy/setup.sh
+++ setup/rcpy/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Setting up pipenv using system pip3..."
-SYSTEM_PIP=/usr/bin/pip3
+SYSTEM_PIP=/usr/local/bin/pip3
 $SYSTEM_PIP install --user pipenv
 ln -sf ~/.local/bin/pipenv ~/bin/pipenv

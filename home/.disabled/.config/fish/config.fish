f6bc319f09daa4dd2520a6d09e8fc575ceb10659
Daniel Goude <daniel@goude.se>
2019-09-20 17:58:04 +0200

    cleanup

--- home/.disabled/.config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-set homeshick_repos $HOME/.homesick/repos
-
-set fish_bind_mode insert
-set fish_key_bindings fish_user_key_bindings
-
-source $homeshick_repos/runcom/aliases
-source $homeshick_repos/homeshick/homeshick.fish
-

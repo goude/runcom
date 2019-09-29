8247381198ebe85f57370ca7225cb62d55c396f7
Daniel Goude <daniel@goude.se>
2019-09-29 18:49:45 +0200

    update

--- utils/notify/notify.sh
+++ utils/notify/notify.sh
@@ -3,6 +3,12 @@
 # Keep track of the drones
 # Don't worry, this will only run if you explicitly tell it to.
 
+RUNCOM_DESCRIPTION='Default description.'
+
+if [ -f "$HOME/.localenv" ]; then
+	source "$HOME/.localenv"
+fi
+
 NOTIFY_NAME=notify-swarm-$(hostname)
 
 data="{"
@@ -13,6 +19,7 @@ data+="\"xip\":\"$(curl -s https://canihazip.com/s)\","
 data+="\"ip\":\"$(hostname -I 2>/dev/null || /usr/sbin/ipconfig getifaddr en1)\","
 data+="\"hostname\":\"$(hostname)\","
 data+="\"df\":\"$(df | grep -E '/$')\","
+data+="\"description\":\"$RUNCOM_DESCRIPTION\","
 data+="\"version\":\"$(git describe --abbrev=0)\""
 data+="}"
 

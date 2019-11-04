ec5ade37d5fa0fd219ef3685b5a5e38cccfaef3c
Daniel Goude <daniel@goude.se>
2019-11-04 21:58:25 +0100

    run black on thxtime.py

--- utils/thxtime/thxtime/thxtime.py
+++ utils/thxtime/thxtime/thxtime.py
@@ -1,17 +1,10 @@
 from __future__ import print_function
-import time
-import random
+
 import datetime
+import random
+import time
 
-
-DICE = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-]
+DICE = ["", "", "", "", "", ""]
 
 
 # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
@@ -28,30 +21,30 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
 
 
 def second_spinner(dt):
-    '''
+    """
     Given a datetime, return a symbol string representing the number
     of seconds.
-    '''
+    """
     index = int(translate(dt.second, 0, 60, 0, 5))
     return DICE[index]
 
 
 def thx_time(dt):
-    ''' Given a datetime, return a string representing the current time.'''
+    """ Given a datetime, return a string representing the current time."""
     data = {
-        '01:17': 'Ezekiel',    # 25 = 01
-        '03:14': 'π',
-        '06:28': 'τ',
-        '11:06': 'Hastings',   # 10:66 = 10:60 + 6 = 11:06
-        '11:38': 'THX',
-        '13:28': 'Big Ben',     # Umm, music
-        '13:37': 'Teel',
-        '15:17': '95LUTH',
-        '22:27': 'Baker St.',  # 1B = 27
+        "01:17": "Ezekiel",  # 25 = 01
+        "03:14": "π",
+        "06:28": "τ",
+        "11:06": "Hastings",  # 10:66 = 10:60 + 6 = 11:06
+        "11:38": "THX",
+        "13:25": "Big Ben",  # Umm, music
+        "13:37": "Teel",
+        "15:17": "95LUTH",
+        "22:27": "Baker St.",  # 1B = 27
     }
 
-    current_time_24 = dt.strftime('%H:%M')
-    current_time_12 = dt.strftime('%I:%M')
+    current_time_24 = dt.strftime("%H:%M")
+    current_time_12 = dt.strftime("%I:%M")
 
     if current_time_24 in data:
         return data[current_time_24]
@@ -68,22 +61,22 @@ def D6(n=3):
 def Dx(x=6, n=3):
     dice = []
     for i in range(n):
-        roll = random.randint(1, 6)
+        roll = random.randint(1, x)
         dice.append(roll)
         # DICE[roll - 1]
-    diestr = ' '.join([DICE[d - 1] for d in dice])
-    return '%s 烈%d' % (diestr, sum(dice))
+    diestr = " ".join([DICE[d - 1] for d in dice])
+    return "%s 烈%d" % (diestr, sum(dice))
 
 
 def thx_time_extended(dt):
-    ''' Given a datetime, return a string representing several time measures.
-    '''
-    #time_str = '冀{}  {}  ({}){}'.format(
-    time_str = 'W{}  {}  ({}){}'.format(
+    """ Given a datetime, return a string representing several time measures.
+    """
+    # time_str = '冀{}  {}  ({}){}'.format(
+    time_str = "W{}  {}  [{}]{}".format(
         dt.now().isocalendar()[1],
         # dt.now().strftime('%m-%d'),
-        dt.now().strftime('%y-%m-%d'),
-        dt.utcnow().strftime('%H'),
+        dt.now().strftime("%Y-%m-%d"),
+        dt.utcnow().strftime("%H"),
         thx_time(dt),
         # D6(3),
         # second_spinner(dt)
@@ -99,5 +92,5 @@ def thx_time_extended_now():
     return thx_time_extended(datetime.datetime.now())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(thx_time_extended_now())

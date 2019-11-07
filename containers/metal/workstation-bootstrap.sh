7c4b192b3e45f24f3e9079d630913f330aba8153
Daniel Goude <daniel@goude.se>
2019-11-07 11:40:16 +0100

    update

---
+++ containers/metal/workstation-bootstrap.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+WORKDIR=~/tmp/bootstrap
+mkdir -p $WORKDIR
+
+echo "obtaining keys..."
+mkdir -p ~/.ssh
+curl https://github.com/goude.keys >> ~/.ssh/authorized_keys
+sort ~/.ssh/authorized_keys | uniq > $WORKDIR/authorized_keys_uniq
+mv -f $WORKDIR/authorized_keys_uniq ~/.ssh/authorized_keys
+chmod og-rwx ~/.ssh/authorized_keys
+
+echo "installing workstation..."
+cd $WORKDIR
+git clone https://github.com/goude/runcom
+
+cd $WORKDIR/runcom/containers/metal/
+sudo ./base-package-setup.sh
+
+cd $WORKDIR/setup
+./update-binary-tools.sh
+./install.sh
+
+cd $WORKDIR
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/FiraCode.zip
+cd .fonts/
+unzip $WORKDIR/FiraCode.zip
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin

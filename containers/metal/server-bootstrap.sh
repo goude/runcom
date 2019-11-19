0b22c397d32e09a1d5cb2e2078b59fa61e5ffac7
Daniel Goude <daniel@goude.se>
2019-11-19 13:00:04 +0100

    update

---
+++ containers/metal/server-bootstrap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+WORKDIR=~/tmp/bootstrap
+mkdir -p $WORKDIR
+
+echo "installing server..."
+cd $WORKDIR
+git clone https://github.com/goude/runcom
+
+cd $WORKDIR/runcom/containers/metal/
+sudo ./base-package-setup.sh
+
+#cd $WORKDIR/runcom/setup
+#./update-binary-tools.sh
+#./runcom-full-install.sh

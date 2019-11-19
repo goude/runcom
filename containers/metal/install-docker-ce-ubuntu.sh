84734731ade6149c95a8de9e2a519327ef3c49f6
Daniel Goude <daniel@goude.se>
2019-11-19 14:41:20 +0100

    update

---
+++ containers/metal/install-docker-ce-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get clean
+
+sudo gpasswd -a mind docker
